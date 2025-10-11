c3f1d8ea5c30995fd698f76fbb44f4b0e685e2f6
Mr-Peach0301 <104506836+Mr-Peach0301@users.noreply.github.com>
2025-10-11 10:14:55 +0800

    Update run_test_ddp.sh

--- MindRec/run_test_ddp.sh
+++ MindRec/run_test_ddp.sh
@@ -1,6 +1,6 @@
 # export WANDB_MODE=disabled
 export CUDA_LAUNCH_BLOCKING=1
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 DATASET=Arts # Games Instruments
 
@@ -9,28 +9,23 @@ declare -A category_length=(
     ["Arts"]=9
     ["Games"]=8
 )
-gen_length_value=${category_length[$DATASET]}
-declare -A gen_length=(
-    ["seqrec_c"]=50
-    ["seqrec_c_simple"]=$gen_length_value
-)
 
 BASE_MODEL=/model/LLaDA-8B-Instruct
 DATA_PATH=../data
 EPOCHS=3
 TEST_BATCH_SIZE=8
 USER_NUM=5000
-TASK=seqrec_c_simple
+TASK=mindrec
 NUM_BEAMS=10
 
 for lr in 5e-5 7e-5 1e-4
 do
     for eps in 0.4 0.5 0.6 0.7
     do
-        GEN_LENGTH=${gen_length[$TASK]}
+        GEN_LENGTH=${category_length[$DATASET]}
         OUTPUT_DIR=./ckpt/${DATASET}_${lr}_${eps}_${EPOCHS}_${TASK}_${USER_NUM}/
         RESULTS_FILE=./results/$DATASET/${lr}_${eps}_${TASK}.json
-        nohup torchrun --nproc_per_node=2 --master_port=4327 test_ddp.py > log.out \
+        nohup torchrun --nproc_per_node=1 --master_port=4327 test_ddp.py > ./out/test.out \
             --ckpt_path $OUTPUT_DIR \
             --base_model $BASE_MODEL\
             --dataset $DATASET \
